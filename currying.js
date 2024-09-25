25cdc9b364a3ae4cf17bfdbb7c6e226f908ff06b
renatakov <renatakov1010@gmail.com>
2024-09-25 21:03:41 +0200

    1

---
+++ currying.js
@@ -0,0 +1,8 @@
+// function multiplyAll(arr) { 
+//     return (i) => {
+//         let newArr = arr.map(item => item * i);
+//         return newArr
+//     }
+// }
+
+// console.log(multiplyAll([2, 3, 4])(2))
